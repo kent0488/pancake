ef0f4d10e084b3d4ede3f66a22838e4858661cb0
kent0488 <32737510+kent0488@users.noreply.github.com>
2017-10-24 16:56:20 +0200

    make a python file

---
+++ hello.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[2]:
+
+print 'hello world!'
+
+"""
+# In[4]:
+
+import pandas as pd
+pd.DataFrame([[1, 'abc'], [2, 'def']], columns = ('id', 'name'))
+
+
+# In[6]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N=10
+x = np.random.rand(N)
+y = np.random.rand(N)
+
+plt.scatter(x, y)
+plt.show()
+"""
